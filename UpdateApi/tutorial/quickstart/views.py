4fc7a6d70026cbfa7edda36d2cf99f408527dae7
ivrabie <vrion69@gmail.com>
2019-11-03 17:37:28 +0200

    Added flask server

--- UpdateApi/tutorial/quickstart/views.py
+++ UpdateApi/tutorial/quickstart/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from quickstart.serializers import UserSerializer, GroupSerializer
-
-
+import os
+import mimetypes
+from django.http import StreamingHttpResponse
+from wsgiref.util import FileWrapper
+from rest_framework.decorators import action
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -11,9 +14,26 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+
+
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class DownloadViewSet(viewsets.ViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+
+    # @action(detail=True, methods=['get'])
+    def list(self,request):
+        the_file = 'd:\\Disertatie\\Disertatie\\gateway\\build\\gateway.bin'
+        filename = os.path.basename(the_file)
+        chunk_size = 2048
+        response = StreamingHttpResponse(FileWrapper(open(the_file, 'rb'), chunk_size),
+                                content_type=mimetypes.guess_type(the_file)[0])
+        response['Content-Length'] = os.path.getsize(the_file)    
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        return response
--- UpdateApi/tutorial/tutorial/urls.py
+++ UpdateApi/tutorial/tutorial/urls.py
@@ -3,9 +3,9 @@ from rest_framework import routers
 from quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-
+router.register(r'users',    views.UserViewSet)
+router.register(r'groups',   views.GroupViewSet)
+router.register(r'download', views.DownloadViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
